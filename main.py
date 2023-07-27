1fe512300e551e0a88657f44e959d17cf30f00af
Sebastián Zapata <63472538+sebastianzapatar@users.noreply.github.com>
2023-07-27 18:16:35 -0500

    Se crea el lexer del compilador

---
+++ main.py
@@ -0,0 +1,11 @@
+from lpp.repl import start_repl
+
+def main() -> None:
+    print('Bienvenido al Lenguaje de Programación de la clase compiladores.')
+    print('Escribe un oración para comenzar.')
+
+    start_repl()
+
+
+if __name__ == '__main__':
+    main()
